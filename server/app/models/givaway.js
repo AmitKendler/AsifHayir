681bb9ba975e7ce4c0bd4539d93e6b0f1cde979e
Guy <glosica3@gmail.com>
2018-03-14 19:44:07 +0200

    added contrillers

--- server/app/models/givaway.js
+++ server/app/models/givaway.js
@@ -2,18 +2,20 @@ const commonModels = require('./commonModels');
 const TimePeriodSchema = commonModels.TimePeriodSchema;
 const ContactSchema = commonModels.ContactSchema;
 const PickupAddressSchema = commonModels.AdderssSchema;
-const AdderssSchema = commonModels.AdderssSchema;
+const AddressSchema = commonModels.AdderssSchema;
 const ProductSchema = require('./product');
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Givaway = new Schema({
+const Giveaway = new Schema({
 	userId: {type: Schema.Types.ObjectId, required: true},
 	title: {type: String, required: true},
 	description: String,
-	address: {type: AdderssSchema, required: true},
+	address: {type: AddressSchema, required: true},
 	pickupTime: TimePeriodSchema,
 	contact: ContactSchema,
 	products: [ProductSchema],
 	status: {type: String, required: true}
 }, {collection: 'givaways'})
+
+mongoose.model("Giveaway", Giveaway);
