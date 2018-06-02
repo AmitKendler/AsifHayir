8030b7b5d24140149fc8af98e01b0857266e2137
AmitKendler <amitkendler1@gmail.com>
2018-06-03 00:11:01 +0300

    creating register page

--- client/app/containers/RegisterContainer/RegisterContainer.js
+++ client/app/containers/RegisterContainer/RegisterContainer.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { Form, Icon, Input, Item, Text } from "native-base";
 import { StyleSheet } from "react-native";
 import { observer, inject } from "mobx-react/native";
-
+import NavbarContainer from "./../NavbarContainer/NavbarContainer";
+import { Col, Row, Grid } from "react-native-easy-grid";
 // TODO add to global styles
 const styles = StyleSheet.create({
     mainLabel: { margin: 15, color: "#333333", fontSize: 18 }
@@ -13,30 +14,66 @@ const styles = StyleSheet.create({
 class RegisterContainer extends Component {
     render() {
         return (
-            <Form>
-				<Text style={styles.mainLabel}>
-					אנא וודא שפרטי איש הקשר שלך נכונים
-				</Text>
-				<Item>
-					<Input
-						placeholder="שם איש הקשר"
-						value={this.props.userStore.contact.name}
-						onChangeText={value =>
-							(giveawayObject.name = value)}
-					/>
-					<Icon name="contact" />
-				</Item>
-				<Item>
-					<Input
-						keyboardType="phone-pad"
-						placeholder="טלפון"
-						value={this.props.userStore.contact.name}
-						onChangeText={value =>
-							(giveawayObject.contact.phone = value)}
-					/>
-					<Icon name="call" />
-				</Item>
-			</Form>
+            <NavbarContainer 
+            		title={"עדכון פרטים אישיים"}
+            		hasCheck
+                    onPressCheck={() => this.props.giveawayStore.postGiveaway()}>
+					<Text style={styles.mainLabel}>
+						לפני שאנחנו מתחילים, אנא הזן את פרטי הקשר שלך
+					</Text>
+					<Item>
+						<Input
+							keyboardType="phone-pad"
+							placeholder="טלפון"
+							value={this.props.userStore.user.phone}
+							onChangeText={value =>
+								(this.props.userStore.user.phone = value)}
+						/>
+						<Icon name="call" />
+					</Item>
+						<Item>
+							<Input
+								keyboardType="text"
+								placeholder="עיר"
+								value={this.props.userStore.user.address.city}
+								onChangeText={value =>
+									(this.props.userStore.user.address.city = value)}
+							/>
+						</Item>
+						<Item>
+							<Input
+								keyboardType="text"
+								placeholder="רחוב"
+								value={this.props.userStore.user.address.streetName}
+								onChangeText={value =>
+									(this.props.userStore.user.address.streetName = value)}
+							/>
+						</Item>
+	            	<Grid>
+					<Col>
+						<Item>
+						<Input
+								keyboardType="number"
+								placeholder="דירה "
+								value={this.props.userStore.user.address.aptNumber}
+								onChangeText={value =>
+									(this.props.userStore.user.address.aptNumber = value)}
+							/>
+						</Item>
+					</Col>
+	            	<Col>
+						<Item>
+						<Input
+								keyboardType="text"
+								placeholder="מספר "
+								value={this.props.userStore.user.address.houseNumber}
+								onChangeText={value =>
+									(this.props.userStore.user.address.houseNumber = value)}
+							/>
+						</Item>
+					</Col>
+					</Grid>
+			</NavbarContainer>
         );
     }
 }
